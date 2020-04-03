e0b3f4e8f7c387c379cde99ca9e80b56dd30774c
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-04-03 21:01:35 +0530

    Enabled data binding

--- app/src/main/java/com/example/databinding/MainActivity.kt
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -8,19 +8,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var name:String?=null
+    private val myname:Myname=Myname("Prateek")
+//    private val mynickname:Myname=Myname("Tiwari")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.bindingname=myname
+//        binding.name=mynickname
         binding.button.setOnClickListener {
             setTextToTextView()
         }
     }
     fun setTextToTextView(){
         binding.apply {
-            name=editText.text.toString()
-            textView2.setText(name)
-            textView3.setText("Data Binding #2")
+            bindingname?.nickname=editText2.text.toString()
+            //textView3.setText("Data Binding #2")
             invalidateAll()
         }
     }
